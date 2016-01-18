b938e1bc2fd1f0b3a6ffaa7406758a6cc378bbdc
NckHmml <NckHmml@users.noreply.github.com>
2016-01-18 18:41:21 +0900

    Finished basic tests and content

---
+++ README.md
@@ -0,0 +1,50 @@
+﻿## Step 1: Adding your App name to the body
+Open public/index.html, in which we all an atribute to the `body` tag.
+
+Modify the tag so the end results looks like this
+```HTML
+<body ng-app="myApp">
+```
+
+This lets Angular know we will be using the App called `MyApp`.
+
+## Step 2: Creating your App in the script
+Navigate to public/app.js, here we will assign the module that will be your App.
+
+In app.js write the following code
+```JavaScript
+var app = angular.module("myApp", []);
+```
+
+This creates a new Angular App called `MyApp`.
+
+## Step 3: Adding the Controller name
+Open public/index.html again, we will place another attribute `ng-controller="MyController"` on a div with the id `main`
+
+The result should look like this.
+```HTML
+<div id="main" 
+     ng-controller="MyController">
+</div>
+```
+
+## Step 4: Creating the Controller
+Navigate back public/app.js, we will add another piece of code.
+```JavaScript
+app.controller("MyController", ["$scope", function ($scope) {
+    $scope.text = "Hello World!";
+}]);
+```
+
+This will create a new Controller called `MyController` in our App. And in it's scope the will be a string called `text` containing `Hello World!`
+
+## Step 5: Writing the text
+Now in public/index.html again, we will add `{{text}}` as content for the div with the id `main`.
+
+The end result should look like this.
+```HTML
+<div id="main" 
+     ng-controller="MyController">
+    {{text}}
+</div>
+```
---
+++ tests.js
@@ -0,0 +1,134 @@
+﻿"use strict";
+
+var env = require("jsdom").env,
+    assert = require("chai").assert,
+    express = require("express"),
+    app = express();
+
+app.use(express.static("public"));
+var listener = app.listen(0);
+
+describe("Step 1", function () {
+    var window;
+    before(function (done) {
+        this.timeout(5000);
+        env({
+            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+            features: {
+                FetchExternalResources: ['script']
+            },
+            done: function (errors, w) {
+                window = w;
+                done();
+            }
+        });
+    });
+
+    it("body must have angular app specified", function () {
+        assert.equal(window.jQuery("body").attr("ng-app"), "myApp");
+    });
+});
+
+describe("Step 2", function () {
+    var window;
+    before(function (done) {
+        this.timeout(5000);
+        env({
+            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+            features: {
+                FetchExternalResources: ['script'],
+                ProcessExternalResources: ['script']
+            },
+            done: function (errors, w) {
+                window = w;
+                done();
+            }
+        });
+    });
+
+    it("angular is running", function () {
+        assert.isDefined(window.angular);
+    });
+
+    it("app is defined", function () {
+        assert.isDefined(window.app);
+    });
+
+    it("app must be an angular module", function () {
+        assert.equal(window.app.name, "myApp");
+    });
+});
+
+describe("Step 3", function () {
+    var window;
+    before(function (done) {
+        this.timeout(5000);
+        env({
+            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+            features: {
+                FetchExternalResources: ['script']
+            },
+            done: function (errors, w) {
+                window = w;
+                done();
+            }
+        });
+    });
+
+    it("main must have controller defined", function () {
+    });
+});
+
+describe("Step 4", function () {
+    var window;
+    before(function (done) {
+        this.timeout(5000);
+        env({
+            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+            features: {
+                FetchExternalResources: ['script'],
+                ProcessExternalResources: ['script']
+            },
+            done: function (errors, w) {
+                window = w;
+                done();
+            }
+        });
+    });
+
+    it("controller must be defined", function () {
+        var scope = window.angular.element(window.document.getElementById("main")).scope();
+        assert.equal(scope.text, "Hello World!");
+    });
+});
+
+describe("Step 5", function () {
+    var window, jQuery;
+    before(function (done) {
+        this.timeout(10000);
+        env({
+            url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+            features: {
+                FetchExternalResources: ['script'],
+                ProcessExternalResources: ['script']
+            },
+            done: function (errors, w) {
+                window = w;
+                env({
+                    url: "http://127.0.0.1:" + listener.address().port + "/index.html",
+                    done: function (errors, w) {
+                        jQuery = require("jquery")(w);
+                        done();
+                    }
+                });
+            }
+        });
+    });
+    it("content must be variable", function () {
+        assert.equal(jQuery("[ng-controller]").html().replace(/\s/g, ''), "{{text}}");
+    });
+
+    it("content must be modified", function () {
+        assert.equal(window.jQuery("[ng-controller]").html().replace(/\s/g, ''), "HelloWorld!");
+    });
+});
